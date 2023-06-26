e7ca6d37d9fb59fd83997ee18e9f478fe5d12172
gxj2003 <2061389208@qq.com>
2023-06-26 17:55:23 +0800

    message=front

---
+++ src/view/LoginView.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Button} from "antd";
+import LoginCard from "../components/LoginView/LoginCard";
+import RegistrationForm from "../components/LoginView/RegistrationForm";
+
+
+class LoginView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showRegistrationForm: false
+        };
+    }
+
+    handleRegistrationSubmit = () => {
+        this.setState({ showRegistrationForm: false });
+    };
+
+    render() {
+        return (
+            <div className="login-page">
+                <div className="login-container">
+                    <div className="login-box">
+
+                        <div className="login-content">
+                            <LoginCard />
+                            {this.state.showRegistrationForm && (
+                                <RegistrationForm onSubmit={this.handleRegistrationSubmit} />
+                            )}
+                            {!this.state.showRegistrationForm && (
+                                <Button onClick={() => this.setState({ showRegistrationForm: true })}>
+                                    Register
+                                </Button>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LoginView;
