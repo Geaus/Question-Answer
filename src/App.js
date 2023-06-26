e499e4b8d1929539ab9881ea255cd3ace99d7f27
illyaks <95566349+illyaks@users.noreply.github.com>
2023-06-26 10:22:41 +0800

    Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/HeaderTest.css';
+import HomePage from "./component/homePage";
+import HeaderTest from "./component/HeaderTest";
+
+function App() {
+  return (
+      <HomePage />
+  );
+}
+
+export default App;
---
+++ src/component/HeaderTest.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../css/HeaderTest.css"
+import {Menu, Input, Avatar, Button} from "antd";
+
+const {Search} = Input;
+
+const menuItems = [
+    {
+        label: "首页",
+        key: "home"
+    },
+    {
+        label: "关注",
+        key: "at"
+    },
+    {
+        label: "热榜",
+        key: "hot"
+    }
+]
+
+const HeaderTest = () => {
+        return (
+            <div className={"header-area-limit"}>
+                <div className={"header-logo-part"} >
+                    <span>Logo</span>
+                </div>
+                <div className={"header-menu-part"}>
+                    <Menu items={menuItems} mode="horizontal"></Menu>
+                </div>
+                <div className={"header-search-part"}>
+                    <Search className={"header-search-input"} />
+                </div>
+                <div className={"header-button-part"}>
+                    <Button className={"header-question-button"} type={"primary"}>提问</Button>
+                </div>
+                <div className={"header-person-part"}>
+                    <Avatar className={"header-avatar-part"} size={"default"}/>
+                    <span className={"header-welcome-font"}>你好，用户</span>
+                </div>
+            </div>
+        )
+}
+
+export default HeaderTest;
